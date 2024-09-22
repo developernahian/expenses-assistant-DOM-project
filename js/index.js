d58795963db0fa9951f293cdf8390cfe10715dae
developernahian <developer.nahian@gmail.com>
2024-09-22 20:58:53 +0600

    history section done

--- js/index.js
+++ js/index.js
@@ -1,3 +1,6 @@
+//global variable
+let count = 0;
+
 //TODO: add event listener for calculate button
 // const calculateButton = document.getElementById('calculate');
 // calculateButton.addEventListener('click', function(){
@@ -6,6 +9,7 @@
 document.getElementById('calculate').addEventListener('click', function () {
     console.log('calculate button clicked');
 
+    count = count + 1;
 
     const income = parseFloat(document.getElementById('income').value);
     const software = parseFloat(document.getElementById('software').value);
@@ -31,6 +35,31 @@ document.getElementById('calculate').addEventListener('click', function () {
     //todo show results section by remove hidden class
     const result = document.getElementById('results')
     result.classList.remove('hidden')
+
+
+
+    //todo history
+    const historyItem = document.createElement('div');
+    historyItem.className  = 'bg-white p-3 rounded-md shadow-sm border-l-2 border-blue-500';
+
+    historyItem.innerHTML = `
+    <p class="text-sm text-gray-700 font-semibold">Serial: ${count}</p>
+    <p class="text-xs text-gray-500">${new Date().toLocaleDateString()}</p>
+    <p class="text-xs text-gray-500">Income: $${income.toFixed(2)}</p>
+    <p class="text-xs text-gray-500">Expenses: $${totalExpenses.toFixed(2)}</p>
+    <p class="text-xs text-gray-500">Balance: $${balance.toFixed(2)}</p>
+    `
+    const historyContainer = document.getElementById('history-list');
+
+
+    //todo insertBefore er maddhome kothay add korbo seta bole dewa jai. 2 ta parameter accept kore. prothom ta ki insert korbo and second ta kothay insert korbo. eta appendChild er moto kaz kore. but appendChile e all time last e add hoy. Note that "prepend" o use kora jai. eta new method
+    /*
+    todo appendChild::: adds the node at the end.
+    todo prepend::: adds the node at the start.
+    todo insertBefore::: allows more control over the exact position by inserting the node before a specific reference node.
+    */
+    historyContainer.insertBefore(historyItem, historyContainer.firstChild);
+
 
 })
 
@@ -74,3 +103,26 @@ document.getElementById('calculate-savings').addEventListener('click', function(
     document.getElementById('savings-amount').innerText = savingAmount.toFixed(2);
     document.getElementById('remaining-balance').innerText = (balance - savingAmount).toFixed(2);
 })
+
+
+//TODO: history tab functionality
+const historyTab = document.getElementById('history-tab');
+const assistantTab = document.getElementById('assistant-tab');
+
+historyTab.addEventListener('click', function(){
+    console.log('history tab clicked');
+
+    historyTab.classList.add('text-white', 'bg-gradient-to-r', 'from-blue-500', 'to-purple-600');
+    historyTab.classList.remove('text-gray-600');
+
+    assistantTab.classList.remove('text-white', 'bg-gradient-to-r', 'from-blue-500', 'to-purple-600');
+    assistantTab.classList.add('text-gray-600');
+
+
+
+    document.getElementById('expense-form').classList.add('hidden')
+    document.getElementById('history-section').classList.remove('hidden')
+
+
+
+});
